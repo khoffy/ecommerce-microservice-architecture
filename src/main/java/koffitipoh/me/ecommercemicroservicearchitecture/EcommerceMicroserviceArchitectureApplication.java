026592f0ecf6f620c686632ce406336932cd0dd9
khoffy <adolf_hermann86@yahoo.fr>
2024-04-23 21:33:54 -0500

    Add Client-ui module

---
+++ src/main/java/koffitipoh/me/ecommercemicroservicearchitecture/EcommerceMicroserviceArchitectureApplication.java
@@ -0,0 +1,13 @@
+package koffitipoh.me.ecommercemicroservicearchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMicroserviceArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceMicroserviceArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/koffitipoh/me/ecommercemicroservicearchitecture/EcommerceMicroserviceArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package koffitipoh.me.ecommercemicroservicearchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMicroserviceArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
