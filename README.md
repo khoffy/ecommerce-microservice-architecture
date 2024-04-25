3aa6497e81b4f570cd9d56971e82c90726948dbd
khoffy <adolf_hermann86@yahoo.fr>
2024-04-24 21:30:44 -0500

    COMPLETE FIRST WORKING VERSION

---
+++ README.md
@@ -0,0 +1,12 @@
+### LIST OF PRODUCTS
+http://localhost:9001/produits    
+
+### LIST OF PRODUCTS THROUGH API GATEWAY
+http://localhost:9103/microservice-produits/produits
+
+### LIST OF PRODUCTS THROUGH CLIENT-UI (and API GATEWAY)
+http://locahost:8080
+
+### EUREKA DASHBOARD
+http://localhost:8761
+
--- client-ui/src/main/java/koffitipoh/me/clientui/configuration/LoadBalancerConfiguration.java
+++ client-ui/src/main/java/koffitipoh/me/clientui/configuration/LoadBalancerConfiguration.java
@@ -12,16 +12,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class LoadBalancerConfiguration {
 
-    @Bean
-    public ServiceInstanceListSupplier discoveryClientServiceInstanceListSupplier(
-            ConfigurableApplicationContext context) {
-        return ServiceInstanceListSupplier.builder()
-                //.withBlockingDiscoveryClient()
-                //.withSameInstancePreference()
-                .withDiscoveryClient()
-                .withHealthChecks()
-                .build(context);
-    }
+//    @Bean
+//    public ServiceInstanceListSupplier discoveryClientServiceInstanceListSupplier(
+//            ConfigurableApplicationContext context) {
+//        return ServiceInstanceListSupplier.builder()
+//                //.withBlockingDiscoveryClient()
+//                //.withSameInstancePreference()
+//                .withDiscoveryClient()
+//                .withHealthChecks()
+//                .build(context);
+//    }
 
 //    @Bean
 //    public ReactiveDiscoveryClient reactiveDiscoveryClient() {
