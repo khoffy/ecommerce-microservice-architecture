b82e3938fb68ac7157012021888176d7deeee846
khoffy <adolf_hermann86@yahoo.fr>
2024-04-26 15:23:34 -0500

    Update security config file

---
+++ microservice-security/src/main/java/koffitipoh/me/msecurity/MsecurityApplication.java
@@ -0,0 +1,15 @@
+package koffitipoh.me.msecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsecurityApplication.class, args);
+    }
+
+}
---
+++ microservice-security/src/test/java/koffitipoh/me/msecurity/MicroserviceSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package koffitipoh.me.msecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
